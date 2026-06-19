62cb80a6aefe678b9804fa6371d855d2edfa8b2e
Priyanka Jajodia <priyanka.jajodia@ibm.com>
2026-06-19 12:15:24 +0530

    Changed app version 2.0

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'); //imports the Express library you just insta
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Release Orchestration Platform!', version: '1.0' }); //when someone hits the root URL, return a JSON message
+  res.json({ message: 'Hello from Release Orchestration Platform!', version: '2.0' }); //when someone hits the root URL, return a JSON message
 });
 
 app.get('/health', (req, res) => {
